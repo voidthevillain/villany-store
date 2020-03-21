bb066ab89ee419c8035cdc6ed9b308cf54fa06f7
Mihai Filip <voidthevillain@users.noreply.github.com>
2020-03-21 12:30:55 +0100

    update menu-items to navigate with withRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      {/* <Link to='/'>Home</Link> */}
+      {/* <button onClick={() => props.history.push('/')}>Home</button> */}
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage  />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+      {/* <HomePage  /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-item.style.scss'
 
 const MenuItem = props => {
-  const { title, imageUrl, size } = props
+  const { title, imageUrl, size, linkUrl, history, match } = props
 
   return (
-    <div className={`menu-item ${size}`}>
+    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         className='background-img'
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -18,4 +19,4 @@ const MenuItem = props => {
   )
 }
 
-export default MenuItem
+export default withRouter(MenuItem)
