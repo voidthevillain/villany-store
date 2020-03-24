1ef0f99d411065ff080b01ac86db890068707a95
Mihai Filip <voidthevillain@users.noreply.github.com>
2020-03-24 12:53:15 +0100

    update the app with reselect, replace cart selectors with memoized selectors

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-import { CartActionTypes } from './cart.types'
+// import { CartActionTypes } from './cart.types'
 
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
